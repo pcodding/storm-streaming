6f7d7a0e516797810a4d0a7c5276bc60278bf8cd
Paul Codding <pcodding@gmail.com>
2013-10-14 15:30:47 -0500

    Updated configuration to include placeholders, and removed unnecessary dependencies.

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mvn compile
 cp src/main/resources/jms* target/classes
-cp src/main/resources/config.properties . 
+cp src/main/resources/config.properties target/classes
 mvn compile exec:java -Dexec.classpathScope=compile -Dexec.mainClass=com.hortonworks.streaming.impl.topologies.TruckEventProcessorTopology
